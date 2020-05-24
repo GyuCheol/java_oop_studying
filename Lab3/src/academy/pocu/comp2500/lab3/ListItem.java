d05887419680a6dde304f5cfaecc3088145a5ffc
rbcjf0219 <rbcjf0219@gmail.com>
2020-05-23 17:43:10 -0700

    lab3

---
+++ Lab3/src/academy/pocu/comp2500/lab3/ListItem.java
@@ -0,0 +1,67 @@
+package academy.pocu.comp2500.lab3;
+
+import java.util.ArrayList;
+
+public class ListItem {
+
+    private String text;
+    private char bulletStyle;
+    private ArrayList<ListItem> subItems = new ArrayList<>();
+
+    public ListItem(String text) {
+        this(text, '*');
+    }
+
+    public ListItem(String text, char bulletStyle) {
+        this.text = text;
+        this.bulletStyle = bulletStyle;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public char getBulletStyle() {
+        return this.bulletStyle;
+    }
+
+    public void setBulletStyle(char bulletStyle) {
+        this.bulletStyle = bulletStyle;
+    }
+
+	public void addSublistItem(ListItem listItem) {
+        subItems.add(listItem);
+    }
+
+    public void removeSublistItem(int index) {
+        this.subItems.remove(index);
+    }
+
+    private String getListText(int depth) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < depth; i++) {
+            sb.append("    ");
+        }
+        
+        sb.append(this.bulletStyle);
+        sb.append(' ');
+        sb.append(this.text);
+        sb.append(System.lineSeparator());
+
+        for (ListItem item : this.subItems) {
+            sb.append(item.getListText(depth + 1));
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return getListText(0);
+    }
+}
